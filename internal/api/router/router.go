2a56949389348f36eb105080e8d58bd6c2f80c0c
hookokoko <648646891@qq.com>
2023-07-05 21:03:38 +0800

    session准备支持

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -19,8 +19,25 @@ func (m *MsgPusher) GetRouter() *gin.Engine {
 	router := gin.New()
 	//router.Use()
 
-	g := router.Group("/message")
-	//g.Use()
+	g := router.Group("/message", func(c *gin.Context) {
+		if c.Request.URL.Path == "authorize" {
+			c.Next()
+		}
+		// 获取session
+
+		// 刷新session的过期时间
+		c.Next()
+	})
+
+	// authorize 提交认证
+	g.POST("authorize", func(c *gin.Context) {
+
+	})
+
+	// unauthorize 解除认证
+	g.POST("unauthorize", func(c *gin.Context) {
+
+	})
 
 	// 发送消息
 	g.POST("send", m.PushHandler.Send)
